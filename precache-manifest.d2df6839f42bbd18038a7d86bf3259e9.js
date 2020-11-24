aaf19c87783954ea280377deca42e562c597a26e
ckr-harsh <xharsh790@gmail.com>
2020-11-24 08:57:51 +0530

    Updates

---
+++ precache-manifest.d2df6839f42bbd18038a7d86bf3259e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c8633f991a46dacb06faacaa1c16a6",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "f1ea0ef9aa8b27557f65",
+    "url": "/Expense-Tracker/static/css/main.8def7541.chunk.css"
+  },
+  {
+    "revision": "6ee3349bb0d228e99a57",
+    "url": "/Expense-Tracker/static/js/2.b1b09979.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expense-Tracker/static/js/2.b1b09979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ea0ef9aa8b27557f65",
+    "url": "/Expense-Tracker/static/js/main.70585d25.chunk.js"
+  },
+  {
+    "revision": "70b87eca44eb76ba3066",
+    "url": "/Expense-Tracker/static/js/runtime-main.3b3fa0ad.js"
+  }
+]);
